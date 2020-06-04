d11bd99d2ffcb04c2c1a9582402849ce0c2784f9
infatigabilis <infatigabilis@gmail.com>
2020-06-05 02:22:10 +1000

    Fix train model jobs

--- src/operation/run-train-turnover-model.sh
+++ src/operation/run-train-turnover-model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Running Train Turnover Model Job..."
-    docker run -v src/resources/production:/home/app/src/resources/production --rm fac-train-turnover-model-job
+    docker run -v $PWD/src/resources/production:/home/app/src/resources/production --rm fac-train-turnover-model-job
     echo "├── Complete"
