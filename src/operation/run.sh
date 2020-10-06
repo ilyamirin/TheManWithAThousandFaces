ed6d4f7988c3527fa9b036b5497f3dd9360796ed
infatigabilis <infatigabilis@gmail.com>
2020-10-06 13:52:37 +1000

    Fix run.sh

--- src/operation/run.sh
+++ src/operation/run.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ ! "$(docker ps -f "name=fac-app" --format '{{.Names}}')" ]; then
     echo "Running App on port ${APP_PORT}..."
-    docker run -p ${APP_PORT}:5000 -d --rm --name fac-app fac-app
+    docker run -p ${APP_PORT}:5000 -d --name fac-app fac-app
     echo "├── Complete"
 else
     echo "App is already running"
@@ -12,7 +12,7 @@ fi
 
 if [ ! "$(docker ps -f "name=fac-ui" --format '{{.Names}}')" ]; then
     echo "Running UI on port ${UI_PORT}..."
-    docker run -p ${UI_PORT}:80 -d --rm --name fac-ui fac-ui
+    docker run -p ${UI_PORT}:80 -d --name fac-ui fac-ui
     echo "├── Complete"
 else
     echo "UI is already running"
